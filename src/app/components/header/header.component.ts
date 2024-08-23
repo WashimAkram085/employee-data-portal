80a9df166ee42cde053fd3cc5b18e450ac0b3e8a
Washim Akram <Washim.Akram@godigit.com>
2024-08-23 15:32:38 +0530

    in navbar user or admin who is logged in showing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component , OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SearchService } from 'src/app/services/search/search.service';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 
 @Component({
@@ -12,13 +13,14 @@ export class HeaderComponent {
 
   userRole: string = 'User';
 
-  constructor(private router: Router, private searchservice : SearchService) {}
+  constructor(private router: Router, private searchservice : SearchService, private employeeservice : EmployeeService) {}
 
   ngOnInit(): void {
     // Logic to determine if the user is an admin or a regular user
     // This can be fetched from a service or local storage
-    const role = localStorage.getItem('role');
-    this.userRole = role ? role : 'User';
+    //const role = localStorage.getItem('role');
+    //this.userRole = role ? role : 'User';
+    this.userRole = this.employeeservice.getLoggedInUser().role.toUpperCase();
   }
 
   logout(): void {
